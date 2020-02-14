a8e1b7046756fbf30d3a8627c6dff0d30d291191
peternjeru <maxp555@gmail.com>
2020-02-14 16:11:44 +0300

    PVP WooCommerce

--- admin/proxyapi-pvp-settings.php
+++ admin/proxyapi-pvp-settings.php
@@ -10,7 +10,12 @@ if (!function_exists( 'proxyapi_pvp_settings_init'))
 {
     function proxyapi_pvp_settings_init()
     {
-        register_setting('general', 'proxyapi_pvp_api_key_value');
+        register_setting('general', 'proxyapi_pvp_api_key_value', array(
+                "type" => "string",
+                "description" => "API Key received from Proxy API Portal",
+                "sanitize_callback" => 'proxyapi_pvp_sanitize_api_key',
+                "show_in_rest" => false
+        ));
 //        register_setting('general', 'proxyapi_pvp_callback_url_value');
 
         add_settings_section(
@@ -34,7 +39,7 @@ if (!function_exists( 'proxyapi_pvp_settings_cb'))
 {
     function proxyapi_pvp_settings_cb()
     {
-        echo '<p>The below section allows integration into <a href="https://proxyapi.co.ke">Proxy API</a></p>';
+        echo '<p>The below section allows integration into <a href="https://proxyapi.co.ke" target="_blank">Proxy API</a></p>';
     }
 }
 
@@ -47,5 +52,24 @@ if (!function_exists( 'proxyapi_pvp_api_key_cb'))
         <?php
     }
 }
+
+if (!function_exists( 'proxyapi_pvp_sanitize_api_key'))
+{
+    function proxyapi_pvp_sanitize_api_key($apiKey)
+    {
+        if (preg_match('/^[\d]{10}\-[\w]{20}$/', $apiKey) !== 1)
+        {
+            add_settings_error(
+                'proxyapi_pvp_api_key_value',
+                'proxyapi_pvp_api_key_value_error',
+                "Invalid API Key",
+                'error'
+            );
+            return null;
+        }
+        return $apiKey;
+    }
+}
+
 
 add_action('admin_init', 'proxyapi_pvp_settings_init');
