aef9b1fdd5d1a2444e81f53a76f0bf56bdcf3eba
peternjeru <maxp555@gmail.com>
2020-02-17 12:50:56 +0300

    Proxy API WooCommerce

--- proxyapi-pvp.php
+++ proxyapi-pvp.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: WooCommerce Pay via ProxyAPI
- * Plugin URI: http://woocommerce.com/products/woocommerce-pay-via-proxyapi/
+ * Plugin Name: Woo Pay via ProxyAPI
+ * Plugin URI: http://woocommerce.com/products/woo-pay-via-proxyapi/
  * Description: Accept Safaricom Lipa na M-Pesa payments using Pay via Proxy API
  * Version: 1.0
  * Author: maxp555
